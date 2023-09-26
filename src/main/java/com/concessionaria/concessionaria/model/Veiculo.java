5203ffce3deafe50f8b67a7ecfd40f013ebd7efc
Filipe Carvalho <filipecarvalhosousa@gmail.com>
2023-09-26 16:38:37 -0300

    criando projeto

---
+++ src/main/java/com/concessionaria/concessionaria/model/Veiculo.java
@@ -0,0 +1,16 @@
+package com.concessionaria.concessionaria.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Veiculo {
+    
+    //#region Atributos
+    public Number veiculo_id;
+    public String veiculo_marca;
+    public String veiculo_modelo;
+    public Number veiculo_ano;
+    //#endregion
+}
